8e1027f39d612e63e512fba4299c11f6745b1e91
K0y-0 <ksam98b.sks726@gmail.com>
2022-10-22 18:34:44 +0900

    スポーンスクリプトの8割完成

--- Assets/Scripts/Sasaki_Scripts/DrawSpawn.cs
+++ Assets/Scripts/Sasaki_Scripts/DrawSpawn.cs
@@ -6,9 +6,12 @@ using UnityEngine;
 public class DrawSpawn : MonoBehaviour
 {
     private Camera mainCamera;
-    [SerializeField]GameObject charas;
-    [SerializeField]float spawnedInterval;
+    [Header("スポーンキャラクターの登録")] [SerializeField]List<GameObject> charas;
+    [Header("キャラクター選択限界数")] [SerializeField]int selectLimit;
+    [SerializeField]List<GameObject> selectedCharas;
+    [Header("スポーン間隔（マウスカーソルの距離）")] [SerializeField]float spawnInterval;
     Vector3 spawnedPosition;
+    int spawnindex = 0;
     bool isSpawned;
  
     void Start()
@@ -34,9 +37,31 @@ public class DrawSpawn : MonoBehaviour
         //     }
         // }
 
+        SelectCharacters();
+
         if(Input.GetMouseButton(0))
         {
             GenerateCharacter(GetMouseRaycastHitPosition());
+        }
+    }
+
+    void SelectCharacters()
+    {
+        if(selectedCharas.Count == selectLimit) return;
+
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedCharas.Add(charas[0]);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            selectedCharas.Add(charas[1]);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            selectedCharas.Add(charas[2]);
         }
     }
 
@@ -66,18 +91,26 @@ public class DrawSpawn : MonoBehaviour
     /// </summary>
     void GenerateCharacter(Vector3 spawnPosition)
     {
-        //Debug.Log();
+        if(selectedCharas.Count() == 0) return;
+
         if(isSpawned)
         {
-            var mouseMoveVelo = GetMouseRaycastHitPosition();
-            if(spawnedPosition.sqrMagnitude > spawnedInterval * spawnedInterval)
+            var mouseMovePos = GetMouseRaycastHitPosition();
+            var mouseInterval = Mathf.Abs(mouseMovePos.sqrMagnitude - spawnedPosition.sqrMagnitude);
+            if(mouseInterval > spawnInterval)
             {
                 isSpawned = false;
             }
         }
         else
         {
-            var obj = Instantiate(charas, spawnPosition, Quaternion.identity);
+            var obj = Instantiate(charas[spawnindex], spawnPosition, Quaternion.identity);
+            if(spawnindex == selectedCharas.Count())
+            {
+                selectedCharas.Clear();
+                spawnindex = 0;
+            }
+            spawnindex++;
             isSpawned = true;
             spawnedPosition = GetMouseRaycastHitPosition();
         }
