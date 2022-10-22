6fd96e30fdcd137c130a34843521f70228b10aa0
Ivan Latypov <paramorphling@gmail.com>
2022-10-22 12:55:54 +0900

    ・タイトル画面からレベル1へ移動を実装

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    public string SceneName;
+
+    public void OnClickLoadScene()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
