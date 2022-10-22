89963b7353bf4fed12ff1744d41dc6b23e88c5ec
K0y-0 <ksam98b.sks726@gmail.com>
2022-10-22 17:26:47 +0900

    3Dモデルの追加、攻撃アニメーションの追加、キャラスポーンスクリプトの修正

--- Assets/Scripts/Sasaki_Scripts/DrawSpawn.cs
+++ Assets/Scripts/Sasaki_Scripts/DrawSpawn.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 public class DrawSpawn : MonoBehaviour
 {
     private Camera mainCamera;
-    private Vector3 currentPosition = Vector3.zero;
     [SerializeField]GameObject charas;
+    [SerializeField]float spawnedInterval;
+    Vector3 spawnedPosition;
+    bool isSpawned;
  
     void Start()
     {
@@ -16,28 +18,81 @@ public class DrawSpawn : MonoBehaviour
  
     void Update()
     {
-        if (Input.GetMouseButton(0)) {
-            var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            var raycastHitList = Physics.RaycastAll(ray).ToList();
-            if (raycastHitList.Any()) {
-                var distance = Vector3.Distance(mainCamera.transform.position, raycastHitList.First().point);
-                var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        // if (Input.GetMouseButton(0)) 
+        // {
+            
+        //     var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        //     var raycastHitList = Physics.RaycastAll(ray).ToList();
+        //     if (raycastHitList.Any())
+        //     {
+        //         var distance = Vector3.Distance(mainCamera.transform.position, raycastHitList.First().point);
+        //         var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
  
-                currentPosition = mainCamera.ScreenToWorldPoint(mousePosition);
-                currentPosition.y = 0;
-                Debug.Log(currentPosition);
-                var obj = Instantiate(charas, currentPosition, Quaternion.identity);
+        //         currentPosition = mainCamera.ScreenToWorldPoint(mousePosition);
+        //         currentPosition.y = 0;
+        //         Debug.Log(currentPosition);
+        //     }
+        // }
+
+        if(Input.GetMouseButton(0))
+        {
+            GenerateCharacter(GetMouseRaycastHitPosition());
+        }
+    }
+
+    /// <summary>
+    /// マウスのポジションを受け取る
+    /// </summary>
+    /// <returns>マウスポジション</returns>
+    Vector3 GetMouseRaycastHitPosition()
+    {
+        var currentPosition = Vector3.zero;
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        var raycastHitList = Physics.RaycastAll(ray).ToList();
+        if (raycastHitList.Any())
+        {
+            var distance = Vector3.Distance(mainCamera.transform.position, raycastHitList.First().point);
+            var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+
+            currentPosition = mainCamera.ScreenToWorldPoint(mousePosition);
+            currentPosition.y = 0;
+        }
+
+        return currentPosition;
+    }
+
+    /// <summary>
+    /// キャラクターの生成
+    /// </summary>
+    void GenerateCharacter(Vector3 spawnPosition)
+    {
+        //Debug.Log();
+        if(isSpawned)
+        {
+            var mouseMoveVelo = GetMouseRaycastHitPosition();
+            if(spawnedPosition.sqrMagnitude > spawnedInterval * spawnedInterval)
+            {
+                isSpawned = false;
             }
         }
+        else
+        {
+            var obj = Instantiate(charas, spawnPosition, Quaternion.identity);
+            isSpawned = true;
+            spawnedPosition = GetMouseRaycastHitPosition();
+        }
+        
+        Debug.Log(spawnedPosition.sqrMagnitude);
     }
  
-    void OnDrawGizmos()
-    {
-        if (currentPosition != Vector3.zero) {
-            Gizmos.color = Color.blue;
-            Gizmos.DrawSphere(currentPosition, 0.5f);
-        }
-    }
+    // void OnDrawGizmos()
+    // {
+    //     if (currentPosition != Vector3.zero)
+    //     {
+    //         Gizmos.color = Color.blue;
+    //         Gizmos.DrawSphere(currentPosition, 0.5f);
+    //     }
+    // }
 }
 
 // ScreenToWorldPoint ScreenPointToRay RaycastHit
