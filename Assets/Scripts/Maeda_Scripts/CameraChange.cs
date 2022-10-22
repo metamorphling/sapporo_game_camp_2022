841fac6b75bada502c5316e1c6b0aa5be972c8f0
Kyousuke-Dayo <112839589+Kyousuke-Dayo@users.noreply.github.com>
2022-10-22 19:12:04 +0900

    カメラ完成

--- Assets/Scripts/Maeda_Scripts/CameraChange.cs
+++ Assets/Scripts/Maeda_Scripts/CameraChange.cs
@@ -10,6 +10,8 @@ public class CameraChange : MonoBehaviour
 
     private int Change = 0;
 
+    Vector3 MouseWheel;
+
     //呼び出し時に実行される関数
     void Start()
     {
@@ -27,27 +29,38 @@ public class CameraChange : MonoBehaviour
     //単位時間ごとに実行される関数
     void Update()
     {
+        MouseWheel.y += Input.mouseScrollDelta.y;
+        
         //スペースキーが押されている間、サブカメラをアクティブにする
-        if (Input.GetKeyDown("space"))
+        if (MouseWheel.y == 1)
         {
             Change++;
+            MouseWheel.y = 0;
+        }
+        if (MouseWheel.y == -1)
+        {
+            Change--;
+            MouseWheel.y = 0;
         }
         if (Change == 0)
         {
             PlayerCamera.SetActive(true);
+            MidCamera.SetActive(false);
             EnemyCamera.SetActive(false);
         }
-        if (Change == 1)
+        if (Change == 1 || Change == -2)
         {
             PlayerCamera.SetActive(false);
             MidCamera.SetActive(true);
+            EnemyCamera.SetActive(false);
         }
-        if (Change == 2)
+        if (Change == 2 || Change == -1)
         {
+            PlayerCamera.SetActive(false);
             MidCamera.SetActive(false);
             EnemyCamera.SetActive(true);
         }
-        if(Change == 3)
+        if (Change == 3 || Change == -3)
         {
             Change = 0;
         }
