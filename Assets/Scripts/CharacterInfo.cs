524c3589ac8fe46da796b46a26a0b187c0c76581
Ivan Latypov <paramorphling@gmail.com>
2022-10-23 16:11:04 +0900

    ・ビルドエラーを対応

--- Assets/Scripts/CharacterInfo.cs
+++ Assets/Scripts/CharacterInfo.cs
@@ -1,11 +1,10 @@
 using System;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 [Serializable]
 public class CharacterInfo
 {
-    public Image Icon;
+    //public Image Icon;
     public GameObject Mesh;
     public GameObject IconBox;
     public bool IsUnlocked;
--- Assets/Scripts/Sasaki_Scripts/DrawSpawn.cs
+++ Assets/Scripts/Sasaki_Scripts/DrawSpawn.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 public class DrawSpawn : MonoBehaviour
